470104e602195985154f623be3a046ae31930ee7
Monty <monty.github@gmail.com>
2021-05-19 10:15:27 +0200

    Apparently I forgot to add the Head to this

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,7 @@ import "98.css";
 import styles from "../styles/Custom404.module.css";
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Custom404() {
   const router = useRouter();
@@ -15,6 +16,37 @@ export default function Custom404() {
 
   return (
     <home>
+      <Head>
+        <title>404 :/</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <meta property="og:image" content="/og_image.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="monty.exe" />
+        <meta property="og:url" content="https://monty.ga" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/og_image.png" />
+      </Head>
       <section>
         <a href="/">
           <div className={styles.blbg}>
