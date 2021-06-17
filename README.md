9c01482867e683df3e19bba91a01a6e6b8e584c6
Monty <monty.github@gmail.com>
2021-06-17 18:10:07 +0200

    Fixed general layout, added About me section

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Check [Redesign](https://github.com/montylion/website/projects/1) for redesign progress.
 
 https://redesign.monty.ga
+
+General layout ~~stolen from~~ inspired by [jonbarrow](https://jonbarrow.dev/)'s website
--- pages/index.js
+++ pages/index.js
@@ -19,38 +19,45 @@ export default function Home() {
               <Link href="#about">About</Link>
             </li>
             <li className={styles.navbarItem}>
-              <Link href="#projects">Projects</Link>
+              <Link href="#stuff">Stuff&#8482;</Link>
             </li>
             <li className={styles.navbarItem}>
               <Link href="#contact">Contact</Link>
             </li>
           </ul>
         </header>
-        <div className={styles.main}>
-          <div className={styles.me}>
-            <Image
-              src="https://via.placeholder.com/300?text=Placeholder"
+        <div className={styles.mainWrapper}>
+          <div className={styles.imageSection}>
+            <img
+              src="/this_is_temporary_and_should_not_be_used_in_the_official_website.jpg"
               alt="Me"
-              width="300px"
-              height="300px"
-              className={styles.meImage}
+              className={styles.imageImage}
             />
-            <div className={styles.meText} id="about">
-              <h1 className={styles.meHeader}>Hi, I'm Monty!</h1>
-              <p className={styles.meParagraph}>
-                Hello! I'm Monty and I make stuff&#8482; in JavaScript. For
-                websites, such as this one, I usually use Next.js.
-              </p>
-              <p className={styles.meParagraph}>
-                I also enjoy localizing open source projects from English to
-                Italian and vice versa.
-              </p>
-              <p className={styles.meParagraph}>
-                I need to think of something else to write here. Bees bees bees
-                bees bees bees bees bees bees bees bees bees bees bees bees bees
-                bees.
-              </p>
-            </div>
+            <h1 className={styles.imageText}>Hi, I'm Monty!</h1>
+          </div>
+
+          <div className={styles.aboutSection} id="about">
+            <h1 className={styles.aboutHeader}>About me</h1>
+            <p className={styles.aboutParagraph}>
+              I make stuff&#8482; in JavaScript & Node.js as a hobby.
+            </p>
+            <p className={styles.aboutParagraph}>
+              Sometimes it's useful stuff&#8482;, sometimes it's just random
+              stuff&#8482; made for fun.
+              <i> Mostly the latter though.</i>
+            </p>
+            <p className={styles.aboutParagraph}>
+              My go to for making websites/APIs is Next.js, a React framework.
+              The very website you're currently browsing is in fact made with
+              Next.js.
+            </p>
+            <p className={styles.aboutParagraph}>
+              I also like localizing open source projects from English to
+              Italian and vice versa.
+            </p>
+          </div>
+          <div className={styles.stuffSection} id="stuff">
+            <h1 className={styles.aboutHeader}>About me</h1>
           </div>
         </div>
       </section>
