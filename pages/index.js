3edfe745bf9f934d03bb46d4435d43553e185cdd
Monty <monty.github@gmail.com>
2021-06-17 10:32:48 +0200

    Created navbar for main page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
-import React, { useEffect } from "react";
+// import Image from "next/image";
+// import React, { useEffect } from "react";
+
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <home>
       <Head>
-        <title>monty.exe</title>
+        <title>Monty</title>
       </Head>
-      <section>Cool beans</section>
+      <section>
+        <header className={styles.navbarBg}>
+          <ul className={styles.navbarList}>
+            <li className={styles.navbarMainItem}>Monty</li>
+            <li className={styles.navbarItem}>
+              <Link href="#about" className={styles.link}>
+                About
+              </Link>
+            </li>
+            <li className={styles.navbarItem}>
+              <Link href="#projects">Projects</Link>
+            </li>
+            <li className={styles.navbarItem}>
+              <Link href="#contact">Contact</Link>
+            </li>
+          </ul>
+        </header>
+      </section>
     </home>
   );
 }
