c4dedb4e3e51c4df930b561dbe34cf7826dc25a3
Monty <git@ashmonty.com>
2021-11-21 20:21:58 +0100

    Add SimpleBar, clean up css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/global.css";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SimpleBar style={{ maxHeight: "100vh" }}>
+      <Component {...pageProps} />
+    </SimpleBar>
+  );
 }
