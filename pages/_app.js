c41324925323530cdde6facbf937b4420579ef4d
Monty <git@ashmonty.com>
2021-11-22 16:30:13 +0100

    fix Webkit stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import "../styles/global.css";
-import SimpleBar from "simplebar-react";
-import "simplebar/dist/simplebar.min.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <SimpleBar style={{ maxHeight: "100vh", overflowX: "hidden" }}>
-      <Component {...pageProps} />
-    </SimpleBar>
-  );
+  return <Component {...pageProps} />;
 }
