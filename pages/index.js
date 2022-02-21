c322a33e9b93213d827d992f9ee819bd44b4536c
Ash Monty <git@ashmonty.com>
2022-02-21 23:09:52 +0100

    misc changes to homepage

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,10 @@ export async function getServerSideProps(context) {
 
   const cards = [
     {
-      title: "The place to dump cool stuff™",
+      title: "Coming soon...",
       caption:
-        "Well, this doesn't exist... yet, but it will be a place for me to dump cool stuff™ (shocking I know)",
-      link: "/the-place-to-dump-cool-stuff",
+        "gimme a sec lol",
+      link: "#stuff",
     },
     {
       title: "Discord Badge",
@@ -43,7 +43,7 @@ export async function getServerSideProps(context) {
     {
       title: "Pretendo Network",
       caption:
-        "I implemented the blog section and the locale switcher, and I designed and wrote the CSS for the account management page and the login page. So yeah basically what I'm saying is that I'm cool. 😎",
+        "Implemented the blog, the locale stuff™, the online Mii editor (not released yet), and also some of the stuff in the homepage, of which the infinite scrolling in the special thanks section is a favorite of mine 😎",
       repo: "PretendoNetwork/website",
       link: "https://pretendo.network",
     },
@@ -276,7 +276,7 @@ export default function Home({ age, cards }) {
                 <title>Twitter</title>
                 <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
               </svg>
-              ashmonty_
+              ashmmonty
             </a>
             <a
               href="mailto:hey@ashmonty.com"
