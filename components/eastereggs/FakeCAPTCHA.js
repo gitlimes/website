d34c72831bbae557bab77256fcbabc12ec6d60c0
Ash Monty <git@ashmonty.com>
2022-02-21 23:10:18 +0100

    minor change to easter egg text

--- components/eastereggs/FakeCAPTCHA.js
+++ components/eastereggs/FakeCAPTCHA.js
@@ -3,9 +3,7 @@ import styles from '../../styles/components/eastereggs/loadingbar.module.css';
 export default function FakeCAPTCHA(context) {
   return (
     <div className={styles.egg}>
-      <h2>Verify you're not a bot</h2>
-      <p>fake captcha here</p>
-      
+      <h2>Still a WIP, sorry lol</h2>
     </div>
   );
 }
