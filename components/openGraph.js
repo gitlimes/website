226194367777f6e319e9571baae33a6df09fe5f9
Ash Monty <git@ashmonty.com>
2022-05-28 14:29:37 +0200

    fix opengraph

--- components/openGraph.js
+++ components/openGraph.js
@@ -42,9 +42,9 @@ export default function OpenGraph() {
       <meta property="og:description" content="I make stuff™ in JavaScript." />
       <meta property="og:url" content="https://ashmonty.com/" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/opengraph.png" />
+      <meta property="og:image" content="https://ashmonty.com/assets/images/twimage.png" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="/opengraph.png" />
+      <meta name="twitter:image" content="https://ashmonty.com/assets/images/twimage.png" />
       {/* Google SEO */}
       <meta name="description" content="I make stuff™ in JavaScript." />
       <meta name="robots" content="index, follow" />
