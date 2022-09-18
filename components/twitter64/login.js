9588ea0792f552c477cc2fd47147e1554527e15d
Ash Monty <git@ashmonty.com>
2022-09-18 12:00:26 +0200

    chore: update twitter handle

--- components/twitter64/login.js
+++ components/twitter64/login.js
@@ -4,13 +4,13 @@ export default function Login({ authUrl }) {
   const tweetTime = new Date("2022-09-07T10:35:32.000Z");
 
   return (
-    <a href="https://twitter.com/ashmlnt/status/1567461559668035584?s=20&t=LgFqtP7Lg3suPr0GzSmd8w" referrerPolicy="no-referrer">
+    <a href="https://twitter.com/ashmdotdev/status/1567461559668035584?s=20&t=LgFqtP7Lg3suPr0GzSmd8w" referrerPolicy="no-referrer">
       <div className={styles.fakeTweet}>
         <div className={styles.tweetHeader}>
           <img alt="Profile picture of my Twitter account" src="https://cdn.discordapp.com/avatars/406125028065804289/4fef71772bccad4a8511784b80416b10.png?size=256" />
           <div className={styles.text}>
             <p className={styles.name}>ash</p>
-            <p className={styles.username}>@ashmlnt</p>
+            <p className={styles.username}>@ashmdotdev</p>
           </div>
         </div>
         <p className={styles.tweetContent}>
