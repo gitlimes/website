a41ac714a476f73ba53e46a0cfecf282e74e4538
Ash Monty <git@ashmonty.com>
2023-03-06 19:43:45 +0100

    feat: add spinmii

---
+++ pages/api/spinmii.js
@@ -0,0 +1,68 @@
+const { GIFEncoder, quantize, applyPalette, nearestColorIndex } = require("gifenc");
+const { createCanvas, loadImage } = require("@napi-rs/canvas");
+
+export default async function stats(req, res) {
+	const {
+		query: { data, axis },
+	} = req;
+
+	const generateRenderUrl = (degrees) => {
+		let cameraRotate = "";
+
+		switch (axis) {
+			case "x":
+				cameraRotate = `&cameraXRotate=${degrees}&cameraYRotate=0&cameraZRotate=0`;
+				break;
+			case "y":
+				cameraRotate = `&cameraXRotate=0&cameraYRotate=${degrees}&cameraZRotate=0`;
+				break;
+			case "z":
+				cameraRotate = `&cameraXRotate=0&cameraYRotate=0&cameraZRotate=${degrees}`;
+				break;
+			default:
+				cameraRotate = `&cameraXRotate=0&cameraYRotate=0&cameraZRotate=0`;
+				break;
+		}
+
+		return `https://studio.mii.nintendo.com/miis/image.png?type=face&expression=normal&width=512&bgColor=13173300&clothesColor=default${cameraRotate}&characterXRotate=0&characterYRotate=0&characterZRotate=0&lightXDirection=0&lightYDirection=0&lightZDirection=0&lightDirectionMode=none&instanceCount=1&instanceRotationMode=model&data=${encodeURIComponent(
+			data
+		)}`;
+	};
+
+	const gif = GIFEncoder();
+	const canvas = createCanvas(512, 512);
+	const ctx = canvas.getContext("2d");
+
+	for (let i = 0; i < 36; i++) {
+		ctx.clearRect(0, 0, 512, 512);
+
+		const frame = generateRenderUrl(i * 10);
+		await loadImage(frame).then((image) => {
+			ctx.drawImage(image, 0, 0);
+		});
+		const { data, width, height } = ctx.getImageData(0, 0, 512, 512);
+
+		const format = "rgba4444";
+
+		const palette = quantize(data, 256, { format });
+		const index = applyPalette(data, palette, format);
+
+		const transparentIndex = nearestColorIndex(palette, [0, 0, 0, 0]);
+		gif.writeFrame(index, width, height, {
+			palette,
+			transparent: true,
+			transparentIndex,
+			delay: 50,
+		});
+	}
+
+	gif.finish();
+
+	const gifBuffer = gif.bytes();
+
+	res.writeHead(200, {
+		"Content-Type": "image/gif",
+		"Content-Length": gifBuffer.length,
+	});
+	res.end(gifBuffer);
+}
---
+++ pages/spinmii.js
@@ -0,0 +1,191 @@
+import Head from "next/head";
+import { useState } from "react";
+import GoHome from "../components/GoHome";
+import Guide from "../components/Guide";
+import OpenGraph from "../components/openGraph";
+import Footer from "../components/Footer";
+
+import styles from "../styles/stuffitempage.module.css";
+
+export default function SpinMii() {
+	const [axis, setAxis] = useState("y");
+	const [miiData, setmiiData] = useState("");
+
+	const renderURL = `/api/spinmii?data=${encodeURIComponent(
+		miiData
+	)}&axis=${encodeURIComponent(axis)}`;
+
+	return (
+		<div>
+			<Head>
+				<title>SpinMii</title>
+				<meta property="og:title" content="spinmii | ash" />
+				<meta property="twitter:title" content="spinmii | ash" />
+				<meta
+					property="og:description"
+					content="Make your mii spin 360° to your heart's content!"
+				/>
+				<meta
+					property="twitter:description"
+					content="Make your mii spin 360° to your heart's content!"
+				/>
+				<meta
+					property="og:image"
+					content="https:/limes.pink/api//spinmii?data=dfe5ecbabfbcc6cc040e0c141c202b363b3940474c53626a818c93a1a7aed5bac2cad6c7cbdfe3f7ff020907081020&axis=y"
+				/>
+				<meta
+					name="twitter:image"
+					content="https:/limes.pink/api//spinmii?data=dfe5ecbabfbcc6cc040e0c141c202b363b3940474c53626a818c93a1a7aed5bac2cad6c7cbdfe3f7ff020907081020&axis=y"
+				/>
+				<OpenGraph />
+			</Head>
+
+			<div className={styles.wrapper} id="wrapper">
+				<GoHome />
+
+				<div className={styles.floatingContainer}>
+					<div className={styles.projectHero}>
+						<h1>
+							Spin<span className="accented">Mii</span>
+						</h1>
+						<p>Make your mii spin 360° to your heart's content!</p>
+					</div>
+
+					<label htmlFor="miiData">
+						Your Mii's Mii Studio <code>data-params</code> (check ? at the top
+						right of this page)
+					</label>
+					<input
+						type="text"
+						required={true}
+						placeholder="Mii Studio data-params"
+						onChange={(e) => setmiiData(e.target.value)}
+					/>
+					<label htmlFor="axis" style={{ marginTop: "1.5rem" }}>
+						Spin axis
+					</label>
+					<fieldset
+						name="axis"
+						className={styles.three}
+						onChange={(e) => setAxis(e.target.value)}
+					>
+						<input type="radio" id="x" name="spin" value="x" />
+						<label htmlFor="x">X</label>
+						<input type="radio" id="y" name="spin" value="y" defaultChecked />
+						<label htmlFor="y">Y</label>
+						<input type="radio" id="z" name="spin" value="z" />
+						<label htmlFor="z">Z</label>
+					</fieldset>
+
+					<a
+						href={renderURL}
+						target="_blank"
+						className={!miiData ? styles.disabled : null}
+					>
+						<button className={styles.styled}>Render</button>
+					</a>
+				</div>
+
+				<div className={styles.guide}>
+					<div id="credits">
+						<Guide icon="info">
+							<h2>Credits</h2>
+							<p>Huge thanks to all the people who made this possible:</p>
+							<ul>
+								<li>
+									<a
+										href="https://github.com/darcellapy/SpinMii"
+										target="_blank"
+										rel="noreferrer noopener"
+										style={{ fontWeight: "bold" }}
+									>
+										le hacker !
+									</a>
+									<img
+										src="/assets/images/le hacker!.png"
+										alt="A drawing of le hacker! with a red circle and arrows pointing at their face. Top right, text that reads: 'the dumbass who had the idea'"
+										style={{ width: "100%" }}
+									/>
+								</li>
+								<li>
+									<a
+										href="#"
+										target="_blank"
+										rel="noreferrer noopener"
+										style={{ fontWeight: "bold" }}
+									>
+										Me
+									</a>
+									, for building the implementation on this page
+								</li>
+							</ul>
+						</Guide>
+					</div>
+					<div
+						id="help"
+						style={{ position: "absolute", top: "0", right: "calc(24px + 1rem)" }}
+					>
+						<Guide icon="help">
+							<h2>
+								How do I get my Mii's <code>data-params</code>?
+							</h2>
+							<h3>Nintendo Network</h3>
+							<ol>
+								<li>
+									Open Mii Studio (login{" "}
+									<a
+										href="https://accounts.nintendo.com/login?post_login_redirect_uri=https%3A%2F%2Faccounts.nintendo.com%2Fmii_studio"
+										target="_blank"
+										rel="noreferrer noopener"
+										style={{ fontWeight: "bold" }}
+									>
+										here
+									</a>
+									)
+								</li>
+								<li>Click on the Mii {">"} Edit</li>
+								<li>
+									Open your browser's console, and paste this:{" "}
+									<code>
+										console.log(document.body.attributes["data-params"].textContent)
+									</code>
+								</li>
+								<li>
+									The console will spit out your mii's <code>data-params</code>!
+								</li>
+							</ol>
+
+							<h3>Pretendo Network</h3>
+							<ol>
+								<li>
+									Open the Mii editor (login{" "}
+									<a
+										href="https://pretendo.network/account/miieditor"
+										target="_blank"
+										rel="noreferrer noopener"
+										style={{ fontWeight: "bold" }}
+									>
+										here
+									</a>
+									)
+								</li>
+								<li>
+									Open your browser's console, and paste this:{" "}
+									<code>
+										console.log(document.querySelector(".new-mii-wrapper{">"}
+										img.new-mii").src.split("&data=")[1])
+									</code>
+								</li>
+								<li>
+									The console will spit out your mii's <code>data-params</code>!
+								</li>
+							</ol>
+						</Guide>
+					</div>
+				</div>
+
+				<Footer sourceCodeOverride="https://github.com/ashmonty/website/blob/main/pages/api/spinmii.js" />
+			</div>
+		</div>
+	);
+}
