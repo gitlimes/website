592286dfd805fbaff1f9f59d3485517e9e69c62d
gitlimes <git@limes.pink>
2023-08-03 21:25:13 +0200

    chore: run image codemod (1)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { Fragment } from "react";
 import { toHumanString } from "human-readable-numbers";
 
