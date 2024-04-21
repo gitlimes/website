0f0b0de784bb2f0ceeb2225d6a9a45be399d0bc6
gitlimes <git@limes.pink>
2024-04-21 17:30:48 +0200

    feat: add anonymous self-hosted analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,35 +3,48 @@ import { useState, useEffect } from "react";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  // we set the state to the darkMode value from localStorage
-  const [darkMode, setDarkMode] = useState(true);
+	// we set the state to the darkMode value from localStorage
+	const [darkMode, setDarkMode] = useState(true);
 
-  // setup
-  useEffect(() => {
-    // if there is no value in localStorage, we set it
-    if (!localStorage.getItem("darkMode")) {
-      localStorage.setItem("darkMode", true);
-    }
-    // we set the state to the value in localStorage
-    setDarkMode(localStorage.getItem("darkMode") === "true");
-  }, []);
+	useEffect(() => {
+		const _mtm = (window._mtm = window._mtm || []);
+		_mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+		(function () {
+			const d = document,
+				g = d.createElement("script"),
+				s = d.getElementsByTagName("script")[0];
+			g.async = true;
+			g.src = "https://matomo.limes.pink/js/container_8X80wgnH.js";
+			s.parentNode.insertBefore(g, s);
+		})();
+	});
 
-  // runs on every theme change
-  function onThemeChange() {
-    document
-      .querySelector("html")
-      .setAttribute("data-theme", darkMode ? "dark" : "light");
+	// setup
+	useEffect(() => {
+		// if there is no value in localStorage, we set it
+		if (!localStorage.getItem("darkMode")) {
+			localStorage.setItem("darkMode", true);
+		}
+		// we set the state to the value in localStorage
+		setDarkMode(localStorage.getItem("darkMode") === "true");
+	}, []);
 
-    localStorage.setItem("darkMode", darkMode);
-  }
+	// runs on every theme change
+	function onThemeChange() {
+		document
+			.querySelector("html")
+			.setAttribute("data-theme", darkMode ? "dark" : "light");
 
-  // we set the data-theme attribute to the body element
-  // depending on the state of the darkMode variable
-  useEffect(() => {
-    onThemeChange();
-  }, [darkMode]);
+		localStorage.setItem("darkMode", darkMode);
+	}
 
-  return (
-    <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
-  );
+	// we set the data-theme attribute to the body element
+	// depending on the state of the darkMode variable
+	useEffect(() => {
+		onThemeChange();
+	}, [darkMode]);
+
+	return (
+		<Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
+	);
 }
