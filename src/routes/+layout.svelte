c396483926375332c0e5052369b96daf8b8a1ed4
limes.pink <git@limes.pink>
2025-01-23 22:46:30 +0100

    chore: tabula rasa, svelte init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
