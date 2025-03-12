eb5f59489ac2e89f45fc0141f5c188a7af6ba0e6
limes.pink <git@limes.pink>
2025-03-12 18:25:18 +0100

    feat: error page. hahahahhaahhahahohdaouhguofsdosdg

---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import * as m from '$lib/paraglide/messages.js';
+
+	import Error from '$lib/components/Error.svelte';
+</script>
+
+<div class="errorwrapper">
+	<a
+		href="/"
+		alt={`${m.meta_error_header()}: HTTP-${page.status}, ${page.error.message}. ${m.meta_error_message()} ${m.meta_error_button()}?`}
+		title={`${m.meta_error_header()}: HTTP-${page.status}, ${page.error.message}. ${m.meta_error_message()} ${m.meta_error_button()}?`}
+	>
+		<Error
+			gameConsole="wiiu"
+			header={`${m.meta_error_header()}: HTTP-${page.status}`}
+			body={`${page.error.message}\n\n${m.meta_error_message()}`}
+			button={m.meta_error_button()}
+		/>
+	</a>
+</div>
+
+<style>
+	.errorwrapper {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+	.errorwrapper a :global(canvas) {
+		max-width: 90vw;
+		max-height: 90vh;
+	}
+</style>
