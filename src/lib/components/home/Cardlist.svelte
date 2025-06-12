7921e2fdfdbf42ca09faaa58c3b54856c9d696d4
limes <git@limes.pink>
2025-06-12 21:42:59 +0200

    fix: diry fix for supabase dying. ideally i'd make a no-stat version of each caption, but i am lazy rn

--- src/lib/components/home/Cardlist.svelte
+++ src/lib/components/home/Cardlist.svelte
@@ -31,7 +31,8 @@
 				<h2>
 					{(m as any)[`stuff_${card.n}_t`]()}
 				</h2>
-				<p>{(m as any)[`stuff_${card.n}_c`]({ val: stats?.[card.n] })}</p>
+				<!-- quick and dirty fix for supabase dying, i am way to tired to do this properly and my website isn't mission critical -->
+				<p>{(m as any)[`stuff_${card.n}_c`]({ val: stats?.[card.n] }).replaceAll(". undefinedk", ". Way too many").replaceAll("undefinedk", "way too many").replaceAll(". undefinedmila", ". Troppi").replaceAll("undefinedmila", "troppi")}</p>
 			</div>
 			<div class="imgwrap" style:background="var(--card-img-bg-{card.n})">
 				<img src={card.i ? `/img/${card.i}` : `/img/${card.n}.svg`} alt="" />
